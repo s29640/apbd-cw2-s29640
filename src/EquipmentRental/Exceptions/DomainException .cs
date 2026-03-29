adb9ac10cb385c790c9dda410236e64a667b8891
Robert Sendys <s29640@pjwstk.edu.pl>
2026-03-29 23:39:38 +0200

    add base domain exception

---
+++ src/EquipmentRental/Exceptions/DomainException .cs
@@ -0,0 +1,7 @@
+﻿namespace EquipmentRental.Exceptions
+{
+    public abstract class DomainException : Exception
+    {
+        protected DomainException(string message) : base(message) { }
+    }
+}
